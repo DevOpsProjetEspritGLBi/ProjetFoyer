33ecb583d87c201fd61463e7a7136f6a367f1a5b
mehdi9990 <131706627+mehdi9990@users.noreply.github.com>
2024-12-21 19:17:00 +0100

    etudiant

---
+++ src/test/java/tn/esprit/tpfoyer/service/EtudiantServiceImplTest.java
@@ -0,0 +1,32 @@
+package tn.esprit.tpfoyer.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EtudiantServiceImplTest {
+
+    @Test
+    void retrieveAllEtudiants() {
+    }
+
+    @Test
+    void retrieveEtudiant() {
+    }
+
+    @Test
+    void addEtudiant() {
+    }
+
+    @Test
+    void modifyEtudiant() {
+    }
+
+    @Test
+    void removeEtudiant() {
+    }
+
+    @Test
+    void recupererEtudiantParCin() {
+    }
+}
